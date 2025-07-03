2734c68242d02cc9d3a2c9d0d7172280fa6b444f
Bodkhe <2408392@cognizant.com>
2025-07-03 16:17:02 +0530

    LoginPage

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -1,5 +1,11 @@
 package pageObjects;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
 /*
  * 1. Handle google button 
  * 2. Switching to google login window
@@ -8,6 +14,34 @@ package pageObjects;
  * 5. Switching back to the main (parent) window.
  */
 
-public class LoginPage {
+public class LoginPage extends BasePage{
 
+	public LoginPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	//locating Elements
+
+	
+	@FindBy(xpath="//*[@type='email']")
+	WebElement email;
+	
+	@FindBy(xpath="//*[@type='password']")
+	WebElement password;
+	
+	
+	//Actions
+	
+	public void setEmail(String Email) {
+		Actions action = new Actions(driver);
+		email.sendKeys(Email);
+		action.keyDown(Keys.ENTER).keyUp(Keys.ENTER).build().perform();
+	}
+	
+	public void setPassword(String Password) {
+		Actions action = new Actions(driver);
+		email.sendKeys(Password);
+		action.keyDown(Keys.ENTER).keyUp(Keys.ENTER).build().perform();
+	}
+	
 }
