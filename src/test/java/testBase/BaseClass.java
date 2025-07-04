73a876ad2dfc96c4800e492b621576918f910c66
Kunj Maheshwari <kunjm@outlook.com>
2025-07-04 10:37:24 +0530

    Added the Base Class

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,5 +1,98 @@
 package testBase;
 
-public class BaseClass {
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Properties;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseClass {
+	public WebDriver driver;
+
+	public Logger logger;
+
+	public Properties p;
+
+	@BeforeClass
+	public void setUp(String os, String br) throws IOException {
+
+		// Loading the config.properties
+		FileReader file = new FileReader(".//src//test//resources//config.properties");
+		p = new Properties();
+		p.load(file);
+
+		logger = LogManager.getLogger(this.getClass());
+
+		// If the execution is Remote in the config.properties file then ->
+		if (p.getProperty("execution_env").equalsIgnoreCase("remote")) {
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+
+			capabilities.setPlatform(Platform.WIN11);
+
+			// OS
+
+			if (os.equalsIgnoreCase("windows")) {
+				capabilities.setPlatform(Platform.WIN11);
+			} else if (os.equalsIgnoreCase("mac")) {
+				capabilities.setPlatform(Platform.MAC);
+			} else {
+				System.out.println("No matching os");
+				return;
+			}
+
+			// BROWSER
+
+			switch (br.toLowerCase()) {
+			case "chrome":
+				capabilities.setBrowserName("chrome");
+				break;
+			case "edge":
+				capabilities.setBrowserName("edge");
+				break;
+			default:
+				System.out.println("Choose a valid browser");
+				return;
+			}
+			
+			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+
+		}
+
+		if (p.getProperty("execution_env").equalsIgnoreCase("local")) {
+			// If the execution is local in the config.properties file then below browser
+			// setup is correct.
+			switch (br.toLowerCase()) {
+			case "chrome":
+				driver = new ChromeDriver();
+				break;
+			case "edge":
+				driver = new EdgeDriver();
+				break;
+			default:
+				System.out.println("Choose a valid browser");
+				return;
+			}
+		}
+
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(p.getProperty("appURL"));
+		driver.manage().window().maximize();
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+	}
 }
