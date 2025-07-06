c2a8fea889edb7bf6ca5eb9c39319dc929ca952a
Gupta <2408684@cognizant.com>
2025-07-06 13:58:39 +0530

    Refactored ExcelUtility, updated UsedCarsPage and pom.xml

--- src/test/java/pageObjects/UsedCarsPage.java
+++ src/test/java/pageObjects/UsedCarsPage.java
@@ -1,9 +1,11 @@
 package pageObjects;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -22,18 +24,50 @@ public class UsedCarsPage {
 	}
 	
 	//Locators
+
+	@FindBy(xpath= "//span[@class='c-p icon-down-arrow']")
+	WebElement more_option;
+
+	@FindBy(xpath= "//a[@data-track-label='nav-used-car']")
+	WebElement car_option;
+	@FindBy(xpath= "//a[@data-url='chennai']")
+	WebElement chennai_option;
+
+//	@FindBy(xpath = "//ul[@class='zw-sr-secLev usedCarMakeModelList popularModels ml-20 mt-10']/li")
+	@FindBy(xpath = "//ul[contains(@class, 'usedCarMakeModelList')]/li/label")
+	List<WebElement> popularModelList;
+
 	@FindBy(xpath="//input[@id='gs_input5']")
-	WebElement ucp_SearchBox;
+	WebElement city_searchBox;
 	
-	@FindBy(xpath="//*[@id=\"ui-id-16\"]")
+	@FindBy(xpath="//*[@id='ui-id-16']")
 	WebElement ucp_CityResultOption;
 	
-	@FindBy(xpath="//ul[@class='zw-sr-secLev usedCarMakeModelList popularModels ml-20 mt-10']//li//label")
-	List<WebElement> ucp_PopularModelList;
-	
 	//Action Methods
+	public void hoverMore(){
+		Actions actions = new Actions(driver);
+		actions.moveToElement(more_option).perform();
+	}
+	public void click_carOption(){
+		car_option.click();
+	}
+	public void clickCity(){
+		chennai_option.click();
+	}
+	public void list_popularModel(){
+		List<String> carNames = new ArrayList<>();
+		for(int i=0;i<popularModelList.size();i++){
+			carNames.add(popularModelList.get(i).getText());
+		}
+		//Printing in console for check
+		for(int i=0;i<popularModelList.size();i++){
+			System.out.println(carNames.get(i));
+		}
+
+	}
+
 	public void enterCity(String city) {
-		ucp_SearchBox.sendKeys(city);
+		city_searchBox.sendKeys(city);
 	}
 	
 	public void selectCity() {
