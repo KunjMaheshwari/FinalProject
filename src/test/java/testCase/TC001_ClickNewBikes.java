a09aa51d81d6faf99b59daf5a5fe044d23504acd
Makkar <2408718@cognizant.com>
2025-07-13 12:08:02 +0530

    Added Stuti Code

---
+++ src/test/java/testCase/TC001_ClickNewBikes.java
@@ -0,0 +1,33 @@
+package testCase;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import testBase.BaseClass;
+
+public class TC001_ClickNewBikes extends BaseClass{
+	
+	@Test
+	public void nav_new_bikes()
+	{
+		//logger.info("Starting TC001_ClickNewBikes");
+		//logger.debug("This is a debug log message");
+		
+		try
+		{
+		HomePage hp=new HomePage(driver);
+		hp.clickNewBike();
+		}
+		catch (Exception e)
+		{
+			//logger.error("Test failed: " + e.getMessage());
+			Assert.fail("Test failed: " + e.getMessage());
+		} 
+		finally 
+		{
+		//logger.info("Finished TC001_ClickNewBikes");
+		}
+	}
+
+}
---
+++ src/test/java/testCase/TC002_UsedCars.java
@@ -0,0 +1,45 @@
+package testCase;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import testBase.BaseClass;
+
+public class TC002_UsedCars extends BaseClass{
+	
+	@Test(priority=1)
+	public void nav_more()
+	{
+		try
+		{
+		HomePage hp=new HomePage(driver);
+		hp.navigateToUsedCars();
+		driver.findElement(By.linkText("Chennai")).click();
+		
+		}
+		catch (Exception e)
+		{
+			//logger.error("Test failed: " + e.getMessage());
+			Assert.fail("Test failed: " + e.getMessage());
+		} 
+		finally 
+		{
+		//logger.info("Finished TC001_ClickNewBikes");
+		}
+	}
+	
+	@Test(priority=2)
+	public void usedCars_Displayed()
+	{
+		if(driver.findElement(By.linkText("Used Cars")).isDisplayed())
+		{
+			Assert.assertTrue(true, "Used Cars are displayed");
+		}
+		else
+		{
+			Assert.fail("Used Cars not Displayed");
+		}
+	}
+}
